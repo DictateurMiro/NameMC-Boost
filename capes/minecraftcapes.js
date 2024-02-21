487f5a3a0c32e1bfd372a743c14b5458ca809025
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-21 15:41:10 +0100

    +

--- capes/minecraftcapes.js
+++ capes/minecraftcapes.js
@@ -1,30 +1,38 @@
 var pageTitle = document.title;
 var username = pageTitle.split('|')[0].trim();
 
-fetch('https://api.capes.dev/load/' + username)
-  .then(response => response.json())
-  .then(data => {
-    // Vérifier si la cape est trouvée
-    if (data.minecraftcapes && data.minecraftcapes.frontImageUrl) {
-        var cards = document.querySelectorAll('.card.mb-3');
-        var lastCard = cards[cards.length - 2];
-        var newCard = document.createElement('div');
-        newCard.className = 'card mb-3';
+let ourSubstring = "Rechercher";
 
-        newCard.innerHTML = `
-            <div class="card-header py-1">
-                <strong>Cape de <a href="https://minecraftcapes.net/" target="_blank" rel="nofollow noopener noreferrer">MinecraftCapes</a></strong>
-            </div>
-            <div class="card-body text-center" id="capeContainer" style="padding: 3px">
-                <img src="${data.minecraftcapes.frontImageUrl}" class="cape-2d align-top skin-button skin-button-selected" alt="Cape MinecraftCapes" width="40px" height="64" style="image-rendering: pixelated;">
-            </div>`;
+if (pageTitle.includes(ourSubstring)) {
+	console.log("Debug (NameMC-Boost) >>> Don't show minecraft cape here")
+} 
 
-        lastCard.parentNode.insertBefore(newCard, lastCard.nextSibling);
-    } else {
-        // La cape n'est pas trouvée: Il faut rien afficher
-        console.log("Debug (NameMC-Boost) >>> no find for MinecraftCapes to", username);
-    }
-  })
-  .catch(error => {
-    console.error('Debug (NameMC-Boost) >>> Error to find cape from MinecraftCapes, api dead ? ', error);
-  });
+else {
+	fetch('https://api.capes.dev/load/' + username)
+	  .then(response => response.json())
+	  .then(data => {
+		// Vérifier si la cape est trouvée
+		if (data.minecraftcapes && data.minecraftcapes.frontImageUrl) {
+			var cards = document.querySelectorAll('.card.mb-3');
+			var lastCard = cards[cards.length - 2];
+			var newCard = document.createElement('div');
+			newCard.className = 'card mb-3';
+
+			newCard.innerHTML = `
+				<div class="card-header py-1">
+					<strong>Cape de <a href="https://minecraftcapes.net/" target="_blank" rel="nofollow noopener noreferrer">MinecraftCapes</a></strong>
+				</div>
+				<div class="card-body text-center" id="capeContainer" style="padding: 3px">
+					<img src="${data.minecraftcapes.frontImageUrl}" class="cape-2d align-top skin-button skin-button-selected" alt="Cape MinecraftCapes" width="40px" height="64" style="image-rendering: pixelated;">
+				</div>`;
+
+			lastCard.parentNode.insertBefore(newCard, lastCard.nextSibling);
+		} else {
+			// La cape n'est pas trouvée: Il faut rien afficher
+			console.log("Debug (NameMC-Boost) >>> no find for MinecraftCapes to", username);
+		}
+	  })
+	  .catch(error => {
+		console.error('Debug (NameMC-Boost) >>> Error to find cape from MinecraftCapes, api dead ? ', error);
+	  });
+}
