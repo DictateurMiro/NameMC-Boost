837ba39a87c0d9e0a5231a3982cf6fc15f910fc6
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 09:59:42 +0100

    +

---
+++ profile-information/name-history/hidehistory.js
@@ -0,0 +1,15 @@
+function hideHistory() {
+    var xpathBase = '/html/body/main/div[2]/div[1]/div[3]/div[2]/table/tbody/tr';
+    var index = 3; // Commencer par le deuxième nom (en supposant que le premier est à l'index 1)
+
+    while (true) {
+        var xpath = `${xpathBase}[${index}]/td[2]`;
+        var xpathResult = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        var element = xpathResult.singleNodeValue;
+
+        if (!element) break;
+
+        element.textContent = '—'; // Remplacer le contenu par un trait, pour être comme sur namemc
+        index += 2; // Passer au prochain nom (en sautant une ligne à chaque fois)
+    }
+}
