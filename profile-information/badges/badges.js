f2ea20ccd6fd6a0de0b1cda456b6a257e86436b3
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 16:54:30 +0100

    +

--- profile-information/badges/badges.js
+++ profile-information/badges/badges.js
@@ -7,3 +7,24 @@ if (document.title.includes("Profil")) {
 } else {
   console.log("Debug (NameMC-Boost) >>> Badges can't show here, we are not in Profile zone");
 }
+
+// Function badgesPrint to show the badges in the profil (22/02/2024)
+// By DictateurMiro
+
+function badgesPrint() {
+  var rows = document.querySelectorAll('.row.g-0');
+  if (rows.length >= 4) {
+    var thirdRow = rows[3];
+    var newRow = document.createElement('div');
+
+    newRow.className = 'row g-0';
+
+    newRow.innerHTML = `
+            <div class="col col-lg-3"><strong>Badges</strong></div>
+            <div class="col-auto" id="show_here_badges_list">Loading ...</div>`;
+
+    thirdRow.parentNode.insertBefore(newRow, thirdRow.nextSibling);
+  } else {
+    console.error("Debug (NameMC-Boost) >>> Badges print can't show up here because the number of rows is less than 3.");
+  }
+}
