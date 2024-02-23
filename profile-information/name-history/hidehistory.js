5e96b75dd15a9503fb5b4ff14b1d224784f46816
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 10:21:26 +0100

    +

--- profile-information/name-history/hidehistory.js
+++ profile-information/name-history/hidehistory.js
@@ -1,3 +1,6 @@
+// Function hideHistory from history profile (22/02/2024)
+// By DictateurMiro
+
 function hideHistory() {
     var xpathBase = '/html/body/main/div[2]/div[1]/div[3]/div[2]/table/tbody/tr';
     var index = 3; // Commencer par le deuxième nom (en supposant que le premier est à l'index 1)
