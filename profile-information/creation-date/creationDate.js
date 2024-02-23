55cca2ba50108335813d8e32c7b08daad9ae3637
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 16:56:21 +0100

    +

--- profile-information/creation-date/creationDate.js
+++ profile-information/creation-date/creationDate.js
@@ -83,3 +83,17 @@ function datePrint(date) {
     console.error("Debug (NameMC-Boost) >>> Badges print can't show up here because the number of rows is less than 3.");
   }
 }
+
+// Function checkStartCreationDate (23/02/2024)
+// By DictateurMiro
+
+function checkStartCreationDate() {
+  if (window.location.href.includes("profile")) {
+    creationDate();
+  } else {
+    console.log("La fonction 'creationDate()' n'est pas exécutée car le titre de la page ne contient pas 'Profil'.");
+  }
+}
+
+// Call function to start
+checkStartCreationDate();
