df0759ef5b18104166a1231228785e186ae2fcd2
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 14:18:45 +0100

    +

---
+++ profile-information/creation-date/check.js
@@ -0,0 +1,13 @@
+// Function checkStartCreationDate (23/02/2024)
+// By DictateurMiro
+
+function checkStartCreationDate() {
+  if (document.title.includes("Profil")) {
+    creationDate();
+  } else {
+    console.log("La fonction 'creationDate()' n'est pas exécutée car le titre de la page ne contient pas 'Profil'.");
+  }
+}
+
+// Call function to start
+checkStartCreationDate();
