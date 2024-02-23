5ef9d48c10c9285f1dca05647576a125442e8a32
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 11:59:58 +0100

    +

---
+++ profile-information/creation-date/formatDate.js
@@ -0,0 +1,13 @@
+// Function formatDate to pickup date (YYYY-MM-DD) to convert to (DD month-in-letter YYYY) (22/02/2024)
+// By DictateurMiro
+
+function formatDate(dateString) {
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  let parts = dateString.split('-'); // split YYYY-MM-DD
+  let year = parts[0];
+  let monthIndex = parseInt(parts[1], 10) - 1;
+  let day = parts[2];
+
+  return `${day} ${months[monthIndex]} ${year}`;
+}
