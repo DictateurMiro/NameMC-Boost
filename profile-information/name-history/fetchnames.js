ee20d3887633063216770870137ec8ac779004f1
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-02-23 09:58:33 +0100

    +

---
+++ profile-information/name-history/fetchnames.js
@@ -0,0 +1,21 @@
+function fetchNames() {
+  return new Promise((resolve, reject) => {
+      var xpathResult = document.evaluate('/html/body/main/div[2]/div[1]/div[1]/div[2]/div[1]/div[3]/samp', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+      var element = xpathResult.singleNodeValue;
+      var uuid = element.innerText;
+
+      let apiUrl = "https://laby.net/api/v3/user/" + uuid + "/names";
+      let proxyUrl = "https://puzzle-verbena-flea.glitch.me/";
+
+    fetch(proxyUrl + apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        let names = data.map(item => item.name).reverse(); // Inverse l'ordre des noms
+        resolve(names);
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des noms:', error);
+        reject(error);
+    });
+  });
+}
