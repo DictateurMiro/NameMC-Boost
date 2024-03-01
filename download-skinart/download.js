d32170efc6b89351376920ae9601b0a0e15f88c2
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-03-01 10:43:22 +0100

    +

---
+++ download-skinart/download.js
@@ -0,0 +1,37 @@
+// Print element in the page of profile NameMC (01/03/2024)
+// By DictateurMiro
+
+function combineCanvasImages() {
+    var div = document.querySelector('div[style="width: 324px; margin: auto; text-align: center;"]');
+    var canvases = div.getElementsByTagName('canvas');
+
+    var combinedCanvas = document.createElement('canvas');
+    var combinedContext = combinedCanvas.getContext('2d');
+
+    var canvasPerRow = 9; // lines number of history skin
+    var rowHeight = 32; // pixel height line
+    var totalWidth = canvasPerRow * 32; // width total
+    var rows = Math.ceil(canvases.length / canvasPerRow); // number lines
+    var totalHeight = rows * rowHeight; // height max
+
+    combinedCanvas.width = totalWidth;
+    combinedCanvas.height = totalHeight;
+
+    var currentX = 0;
+    var currentY = 0;
+    for (let i = 0; i < canvases.length; i++) {
+        if (i > 0 && i % canvasPerRow === 0) {
+            currentY += rowHeight;
+            currentX = 0;
+        }
+        combinedContext.drawImage(canvases[i], currentX, currentY);
+        currentX += canvases[i].width;
+    }
+
+    var downloadLink = document.createElement('a');
+    downloadLink.href = combinedCanvas.toDataURL();
+    downloadLink.download = 'combined_image.png';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
